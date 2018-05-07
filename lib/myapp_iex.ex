7298da04819b3036282a97fba2b2f7b201aa521c
Kanta.Takeuchi <kanta.takeuchi@y5093873.tok.access-company.com>
2018-05-07 15:29:14 +0900

    Add CLI

--- lib/myapp_iex.ex
+++ lib/myapp_iex.ex
@@ -3,6 +3,30 @@ defmodule MyappIex do
   Documentation for MyappIex.
   """
 
+  use Application
+
+    def start(_type, _args) do
+      MyappIex.Supervisor.start_link()
+    end
+
+    alias Hipchat.ApiClient
+    alias Hipchat.Httpc.Response
+    alias Hipchat.V2.Rooms
+
+    def main([room_id, msg]) do
+      client = ApiClient.new(load_token())
+      {:ok, %Response{status: 201}} = Rooms.send_message(client, room_id, %{message: msg})
+    end
+
+    defp load_token() do
+      Path.expand("~/.config/hipchat_cli/token")
+      |> File.read!()
+      |> String.trim()
+    end
+
+
+
+
   @doc """
   Hello world.
 
@@ -12,7 +36,7 @@ defmodule MyappIex do
       :world
 
   """
-  def main(_argv)  do
-    IO.puts("Hello!")
+  def hello do
+      :world
+    end
   end
-end
